b99977200d715268e0302ff7630a9504684da482
Maximus <85298325+MaximusPython@users.noreply.github.com>
2025-08-07 21:15:20 +0300

    add dynamic module user

--- apps/user-api/src/adapters/http-adapter/src/module/users.module.ts
+++ apps/user-api/src/adapters/http-adapter/src/module/users.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { UsersController } from '../controllers/users.controller';
 import { DatabaseService } from '@app/module-api/service/database.service';
 
-@Module({
-  controllers: [UsersController],
-  providers: [DatabaseService],
-})
-export class UsersModule {}
+@Module({})
+export class UsersModule {
+  static register(): DynamicModule {
+    return {
+      module: UsersModule,
+      controllers: [UsersController],
+      providers: [DatabaseService],
+      global: true,
+    };
+  }
+}
