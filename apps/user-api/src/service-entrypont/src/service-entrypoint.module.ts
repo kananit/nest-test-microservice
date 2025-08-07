7012f10d08a276803ae64362c75f4c8fb6bfa977
Maximus <85298325+MaximusPython@users.noreply.github.com>
2025-08-07 15:52:24 +0300

    add dynamics modules

--- apps/user-api/src/service-entrypont/src/service-entrypoint.module.ts
+++ apps/user-api/src/service-entrypont/src/service-entrypoint.module.ts
@@ -4,6 +4,6 @@ import { DatabaseModule } from '@app/module-api/module/database.module';
 import { AppModule } from '../../core/application-module/src/module/app.module';
 
 @Module({
-  imports: [DatabaseModule, AppModule],
+  imports: [DatabaseModule.register(), AppModule.register()],
 })
 export class ServiceEntrypointModule {}
