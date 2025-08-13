ef6a5af0c19cd9ed0e9ab90eedb552fa663382c0
Maximus <85298325+MaximusPython@users.noreply.github.com>
2025-08-13 14:22:52 +0300

    update

--- apps/user-api/src/adapters/http-adapter/src/controllers/users.controller.ts
+++ apps/user-api/src/adapters/http-adapter/src/controllers/users.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, Get, Post, Body, Put, Delete } from '@nestjs/common';
-import { CreateUserDto } from '../dto/create-user.dto';
-import { UpdateUserDto } from '../dto/update-user.dto';
-import { DeleteUserDto } from '../dto/delete-user.dto';
+import { UserDto } from '../dto/user.dto';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import {
   CreateUserResult,
   DeleteUserResult,
   UpdateUserResult,
   User,
-} from '../../../../../../../libs/module-postgres/src/types/user.interfaces';
+} from '@app/module-postgres/types/user.interfaces';
+
 import { UsersService } from 'apps/user-api/src/core/application-module/src/service/user-service';
 @Controller('users')
 @ApiTags('Users')
@@ -16,7 +15,7 @@ export class UsersController {
   constructor(private readonly userService: UsersService) {}
   @Post()
   @ApiResponse({ status: 201 })
-  async createUser(@Body() dto: CreateUserDto): Promise<CreateUserResult> {
+  async createUser(@Body() dto: UserDto): Promise<CreateUserResult> {
     return await this.userService.createUser(dto);
   }
   @Get()
@@ -24,11 +23,11 @@ export class UsersController {
     return await this.userService.findAll();
   }
   @Delete()
-  async deleteUserById(@Body() dto: DeleteUserDto): Promise<DeleteUserResult> {
+  async deleteUserById(@Body() dto: UserDto): Promise<DeleteUserResult> {
     return await this.userService.deleteUserById(dto);
   }
   @Put()
-  async updateUserById(@Body() dto: UpdateUserDto): Promise<UpdateUserResult> {
+  async updateUserById(@Body() dto: UserDto): Promise<UpdateUserResult> {
     return await this.userService.updateUserById(dto);
   }
 }
