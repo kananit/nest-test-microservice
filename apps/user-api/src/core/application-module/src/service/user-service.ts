28a3a261526641f59d84ecb537a59122fcc80067
Maximus <85298325+MaximusPython@users.noreply.github.com>
2025-08-19 11:27:33 +0300

    update

--- apps/user-api/src/core/application-module/src/service/user-service.ts
+++ apps/user-api/src/core/application-module/src/service/user-service.ts
@@ -8,6 +8,7 @@ import {
   User,
 } from '@app/module-postgres/types/user.interfaces';
 import { DatabaseService } from '@app/module-postgres/service/database.service';
+import { RabbitmqService } from '@app/rabbitmq';
 
 @Injectable()
 export class UsersService {
